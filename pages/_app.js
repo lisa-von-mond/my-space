69dc44e837ced9192eb3be45453c9605128dbb45
lisa-von-mond <lisalue@posteo.de>
2022-09-14 15:35:26 +0200

    make color change background

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { useState } from 'react'
+import styled, {css} from 'styled-components'
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+
+  return (
+  <Component {...pageProps} />)
+
 }
 
 export default MyApp
+
+
+/*background-image: linear-gradient(to top, ${props => props.bgone} 1%, ${props => props.bgtwo} 100%);*/
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ import { Header } from '../components/header'
 import { PrettyElement } from '../components/element-component'
 import { cardData } from '../card-data'
 import { useState } from 'react'
+import styled from 'styled-components'
+import { motion } from 'framer-motion'
+
 
 
 export default function Home() {
@@ -13,6 +16,10 @@ const [spaceFilter, setSpaceFilter] = useState(0)
 
 const currentFilter = cardData.filter((element)=>(element.cat.includes(spaceFilter) === true))
 
+const myvariants = {
+  one: { opacity: 1 },
+  two: { opacity: 0 },
+}
 
   return (
     <div className={styles.container}>
@@ -21,6 +28,36 @@ const currentFilter = cardData.filter((element)=>(element.cat.includes(spaceFilt
         <meta name="lisa's portfolio" content="" />
         <link rel="icon" href="" />
       </Head>
+
+      <BGblue as={motion.div}
+    variants={myvariants}
+    animate={spaceFilter === 0 ? "one" : "two"}
+    transition={{ duration: 2 }}>
+      </BGblue>
+
+      <BGpink as={motion.div}
+    variants={myvariants}
+    animate={spaceFilter === 1 ? "one" : "two"}
+    transition={{ duration: 2 }}>
+      </BGpink>
+
+      <BGgold as={motion.div}
+    variants={myvariants}
+    animate={spaceFilter === 2 ? "one" : "two"}
+    transition={{ duration: 2 }}>
+      </BGgold>
+
+      <BGgrey as={motion.div}
+    variants={myvariants}
+    animate={spaceFilter === 3 ? "one" : "two"}
+    transition={{ duration: 2 }}>
+      </BGgrey>
+
+      <BGyellow as={motion.div}
+    variants={myvariants}
+    animate={spaceFilter === 4 ? "one" : "two"}
+    transition={{ duration: 2 }}>
+      </BGyellow>
 
       <Header spaceFilter={spaceFilter} setSpaceFilter={setSpaceFilter}/>
 
@@ -45,3 +82,65 @@ const currentFilter = cardData.filter((element)=>(element.cat.includes(spaceFilt
     </div>
   )
 }
+
+
+const BGpink = styled.div`
+height: 120vh;
+width:120vw;
+z-index:0;
+position:fixed;
+top:-10vh;
+left:-10vw;
+background: pink;
+`
+
+const BGblue = styled.div`
+height: 120vh;
+width:120vw;
+z-index:0;
+position:fixed;
+top:-10vh;
+left:-10vw;
+background: skyblue;
+`
+
+const BGyellow = styled.div`
+height: 120vh;
+width:120vw;
+z-index:0;
+position:fixed;
+top:-10vh;
+left:-10vw;
+background: yellow;
+`
+
+const BGgrey = styled.div`
+height: 120vh;
+width:120vw;
+z-index:0;
+position:fixed;
+top:-10vh;
+left:-10vw;
+background: lightgrey;
+`
+
+const BGgold = styled.div`
+height: 120vh;
+width:120vw;
+z-index:0;
+position:fixed;
+top:-10vh;
+left:-10vw;
+background: goldenrod;
+`
+
+const SwitchButton = styled.div`
+height: 3rem;
+width: 3rem;
+position:fixed;
+top:20vh;
+left:20vw;
+background: white;
+bborder-radius: 50&;
+z-index: 50;
+`
